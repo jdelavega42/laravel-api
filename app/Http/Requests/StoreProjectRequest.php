5de181b4fb4fd75f1f8be2407265b5293aeab716
“jdelavega94” <jcdelavega94@gmail.com>
2023-06-07 20:25:59 +0200

    add type option in create.blade

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -26,7 +26,8 @@ class StoreProjectRequest extends FormRequest
     {
         return [
             'title' => ['required', 'max:50'],
-            'description' => 'nullable'
+            'description' => 'nullable',
+            'type_id' => 'nullable'
         ];
     }
 }
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -10,7 +10,15 @@
             <label for="description" class="form-label">Descrizione</label>
             <textarea class="form-control" id="description" rows="3" name="description">{{ old('description') }}</textarea>
         </div>
-
+        <div class="mb-3">
+            <label for="type">Tipologia</label>
+            <select class="form-select" name="type_id" id="type" >
+                <option value=""></option>
+                @foreach ($types as $type)
+                <option @selected(old('type_id') == $type->id) value="{{$type->id}}">{{$type->name}}</option>
+                @endforeach
+            </select>
+        </div>
         <button class="btn btn-secondary" type="submit">invia</button>
 
     </form>
